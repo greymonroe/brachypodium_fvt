a4f9313ec55ab8c50672f9ea6137581b1bd8bef9
greymonroe <greymonroe@gmail.com>
2020-03-05 18:14:13 +0100

    minor changes

--- src/maps.R
+++ src/maps.R
@@ -9,8 +9,8 @@ bdist<-rasterGrob(readJPEG("../Figures/bdist.jpg"), interpolate=TRUE)
 map_bsylv<-rasterGrob(readPNG("../Figures/mab_bsylv.png"), interpolate=TRUE)
 bsylv<-rasterGrob(readJPEG("../Figures/bsylv.jpeg"), interpolate=TRUE)
 
-maps<-plot_grid(map_bdist, map_bsylv, labels = c("a","c"), ncol = 1)
-pics<-plot_grid(bdist, bsylv, labels = c("b","d"), ncol = 1, hjust = -1)
+maps<-plot_grid(map_bdist, map_bsylv, labels = c("a","b"), ncol = 1)
+pics<-plot_grid(bdist, bsylv, labels = c("c","d"), ncol = 1, hjust = -1)
 
 plot<-plot_grid(maps, pics, ncol=2, rel_widths = c(1, .25), label_size = 8)
 print(plot)
